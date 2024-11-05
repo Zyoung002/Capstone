d48ccc2394d10da231aca11788ccda1e00cc0a5d
Zachary Young <zachary.young1030@gmail.com>
2024-11-04 20:40:08 -0500

    Fixed form Submission and added validation

--- scripts.js
+++ scripts.js
@@ -12,4 +12,58 @@ function storeInput() {
         console.log(date);
     })
 }
-console.log('date', 'address', 'summary')
+
+const date = document.getElementById('date');
+
+date.addEventListener('change', (event) =>{
+
+    const selectedDate = event.target.value;
+    let date = document.getElementById('date').value;
+    console.log(date);
+})
+// code capture form submission with out page turn over and log it out//
+
+
+
+let postsubmit = document.getElementById("inputs");
+
+    postsubmit.addEventListener("submit", (d) => {
+        d.preventDefault();
+
+        let date = document.getElementById("date");
+        let address = document.getElementById("address");
+        let summary = document.getElementById('summary');
+
+        if (address.value == "" || summary.value == "") {
+            alert("Ensure you have filled out all fields!")
+        } else {
+            alert("Your data has been added to the feed!");
+            console.log(`This form has date of ${date.value} and a location of ${address.value}`);
+        }    
+        
+    })
+
+    function validateform() {
+        let x = documents.forms["inputs"]["address"].value;
+        if (x =="") {
+            alert("Address required");
+            return false;
+        }
+        let y = documents.forms["inputs"]["summary"].value;
+        if (y =="") {
+            alert("Brief Summary required");
+            return false;
+        }
+    
+    }
+
+
+    document.getElementById('inputs').addEventListener('submit', (postsubmit)) 
+    {
+        
+
+        const formData = new FormData(document.getElementById('inputs'));
+        const inputValue = formData.get(postsubmit);
+        document.getElementById('feeddisplay').textContent = inputValue;
+
+    }
